6da13a53e25c778f82cd07aeb68f6574ac117351
marko <marko@nikovic.com>
2025-07-12 08:37:45 -0400

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
+
   server: {
     host: '0.0.0.0'
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
